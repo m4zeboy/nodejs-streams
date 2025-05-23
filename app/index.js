104264d36812894dc6cb03ad35c5ac42ebeb10e3
m4zeboy <moises.azevedo@ufms.br>
2025-05-22 23:30:25 -0300

    feat: initialize server with CSV to JSON streaming functionality
    - Added an HTTP server in index.js that streams data from a CSV file (animeflv.csv) and converts it to JSON format using the csvtojson library.
    - Implemented CORS headers to allow cross-origin requests.
    - Set up a pipeline to read, transform, and write data in a non-blocking manner.
    - Created package.json and pnpm-lock.yaml to manage dependencies and scripts for development.

---
+++ app/index.js
@@ -0,0 +1,69 @@
+const API_URL = "http://localhost:3000";
+
+async function consumeAPI(signal) {
+  const response = await fetch(API_URL, {
+    signal,
+  });
+  let counter = 0;
+  const reader = response.body
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(parseNDJSON());
+  // .pipeTo(
+  //   new WritableStream({
+  //     write(chunk) {
+  //       console.log(++counter, chunk);
+  //     },
+  //   })
+  // );
+
+  return reader;
+}
+
+function appendToHTML(element) {
+  return new WritableStream({
+    write({ title, description, url_anime }) {
+      element.innerHTML += `
+      <article> 
+          <div class="text">
+            <h3>${title}</h3>
+            <p>${description}</p>
+            <a href="${url_anime}">Clique para assistir</a>
+          </div>
+        </article>
+        `;
+    },
+  });
+}
+
+// essa função vai se certifcar que caso dois chunks chegem em uma unica transmissão,
+// converta corretamente para JSON
+// dado: {}\n{}
+// converte:
+//        {}
+//        {}
+function parseNDJSON() {
+  let ndjsonBuffer = "";
+  return new TransformStream({
+    transform(chunk, controller) {
+      ndjsonBuffer += chunk;
+      const items = ndjsonBuffer.split("\n");
+      items
+        .slice(0, -1)
+        .forEach((item) => controller.enqueue(JSON.parse(item)));
+
+      ndjsonBuffer = items[items.length - 1];
+    },
+    flush(controller) {
+      if (!ndjsonBuffer) return;
+      controller.enqueue(JSON.parse(ndjsonBuffer));
+    },
+  });
+}
+
+const [start, stop, cards] = ["start", "stop", "cards"].map((item) =>
+  document.getElementById(item)
+);
+const abortController = new AbortController();
+const readable = await consumeAPI(abortController.signal);
+
+readable.pipeTo(appendToHTML(cards));
---
+++ server/index.js
@@ -0,0 +1,61 @@
+import csvtojson from "csvtojson";
+import { createReadStream } from "node:fs";
+import { createServer } from "node:http";
+import { Readable, Transform } from "node:stream";
+import { WritableStream } from "node:stream/web";
+import { setTimeout } from "node:timers/promises";
+
+const PORT = 3000;
+// curl -i -X OPTIONS -N localhost:3000
+// curl -N localhost:3000
+createServer(async (req, res) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+  };
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204, headers);
+    res.end();
+    return;
+  }
+  req.once("close", (_) => console.log("connection was closed", items));
+  let items = 0;
+  Readable.toWeb(createReadStream("./animeflv.csv"))
+    // o passo a passo que cada item individual vai trafegar
+    .pipeThrough(Transform.toWeb(csvtojson()))
+    .pipeThrough(
+      new TransformStream({
+        transform(chunk, controller) {
+          const data = JSON.parse(Buffer.from(chunk));
+          const mappedData = {
+            title: data.title,
+            description: data.description,
+            url_anime: data.url_anime,
+          };
+          // quqbra de linha porque é um NDJSON
+          controller.enqueue(JSON.stringify(mappedData).concat("\n"));
+        },
+      })
+    )
+    // pipeTo é a ultima etapa do pipeline
+    .pipeTo(
+      new WritableStream({
+        async write(chunk) {
+          await setTimeout(100);
+          items++;
+          res.write(chunk);
+        },
+        close() {
+          res.end();
+        },
+      })
+    );
+
+  res.writeHead(200, headers);
+  // res.end("ok2")
+})
+  .listen(PORT)
+  .on("listening", (_) =>
+    console.log(`Server is running on http://localhost:${PORT}`)
+  );
